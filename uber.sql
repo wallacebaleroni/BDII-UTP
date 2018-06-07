165635366ac692df8b15e7e3e4b8a3cb0b7e976e
Júlia Falcão <juliafsfalcao@hotmail.com>
2018-06-06 22:07:22 -0300

    organizei coisinhas

--- uber.sql
+++ uber.sql
@@ -1,19 +1,22 @@
 DROP TABLE IF EXISTS Passageiro CASCADE;
-CREATE TABLE Passageiro(
-	cpf VARCHAR NOT NULL,
+CREATE TABLE Passageiro (
+	cpf CHAR(11) NOT NULL, -- checar
 	nome VARCHAR NOT NULL,
-	email VARCHAR NOT NULL,
-	telefone INT NOT NULL,
+	email VARCHAR NOT NULL, -- checar
+	telefone INT NOT NULL, -- checar
+	nota INT DEFAULT 0, -- calcular
+	endereco_casa VARCHAR,
+	endereco_trabalho VARCHAR,
 	
 	CONSTRAINT PK_Passageiro PRIMARY KEY (cpf)
 );
 
 DROP TABLE IF EXISTS Motorista CASCADE;
-CREATE TABLE Motorista(
-	cpf VARCHAR NOT NULL,
+CREATE TABLE Motorista (
+	cpf VARCHAR NOT NULL, -- checar
 	nome VARCHAR NOT NULL,
-	email VARCHAR NOT NULL,
-	telefone INT NOT NULL,
+	email VARCHAR NOT NULL, -- checar
+	telefone INT NOT NULL, -- checar
 	carro INT NOT NULL,
 	
 	CONSTRAINT PK_Motorista PRIMARY KEY (cpf),
@@ -21,12 +24,12 @@ CREATE TABLE Motorista(
 );
 
 DROP TABLE IF EXISTS Carro CASCADE;
-CREATE TABLE Carro(
+CREATE TABLE Carro (
 	renavam INT NOT NULL,
-	placa VARCHAR NOT NULL,
+	placa CHAR(7) NOT NULL, -- checar
 	marca VARCHAR NOT NULL,
 	modelo VARCHAR NOT NULL,
-	ano INT NOT NULL,
+	ano INT NOT NULL, -- <= 2018
 	categoria INT NOT NULL,
 	
 	CONSTRAINT PK_Carro PRIMARY KEY (renavam),
@@ -34,7 +37,7 @@ CREATE TABLE Carro(
 );
 
 DROP TABLE IF EXISTS Categoria CASCADE;
-CREATE TABLE Categoria(
+CREATE TABLE Categoria (
 	id INT NOT NULL,
 	nome VARCHAR NOT NULL,
 	
@@ -42,16 +45,16 @@ CREATE TABLE Categoria(
 );
 
 DROP TABLE IF EXISTS Corrida CASCADE;
-CREATE TABLE Corrida(
+CREATE TABLE Corrida (
 	passageiro VARCHAR NOT NULL,
 	motorista VARCHAR NOT NULL,
 	categoria INT NOT NULL,
-	time_ini TIMESTAMP NOT NULL,
+	time_inicio TIMESTAMP NOT NULL,
 	time_fim TIMESTAMP NOT NULL,
-	end_ini VARCHAR NOT NULL,
-	end_fim VARCHAR NOT NULL,
+	end_origem VARCHAR NOT NULL,
+	end_destino VARCHAR NOT NULL,
 	
-	CONSTRAINT PK_Corrida PRIMARY KEY (passageiro, time_ini),
+	CONSTRAINT PK_Corrida PRIMARY KEY (passageiro, time_inicio),
 	CONSTRAINT FK_Passageiro FOREIGN KEY (passageiro) REFERENCES Passageiro (cpf),
 	CONSTRAINT FK_Motorista FOREIGN KEY (motorista) REFERENCES Motorista (cpf),
 	CONSTRAINT FK_Categoria FOREIGN KEY (categoria) REFERENCES Categoria (id)
@@ -61,22 +64,22 @@ INSERT INTO Passageiro VALUES('123.456.789-10', 'Joao', 'joao@email.com', 266969
 INSERT INTO Passageiro VALUES('987.654.321-00', 'Maria', 'maria@email.com', 26696969);
 INSERT INTO Passageiro VALUES('696.969.696.96', 'Rogerinho', 'djrpdfdtpe1395271@hotmail123.com', 964865952);
 
-INSERT INTO Categoria VALUES(1, 'Uber X');
-INSERT INTO Categoria VALUES(2, 'Uber Black');
+INSERT INTO Categoria VALUES(1, 'UberX');
+INSERT INTO Categoria VALUES(2, 'UberBlack');
+INSERT INTO Categoria VALUES(3, 'Select');
 
 INSERT INTO Carro VALUES(1234, 'CAM2010', 'Chevrolet', 'Camaro', 2010, 1);
 INSERT INTO Carro VALUES(5678, 'PSC2018', 'Porsche', '718', 2018, 2);
 
-INSERT INTO Motorista VALUES('102.938.475-60', 'Jorge', 'jorge@bol.com', 12443857, 1234);
-INSERT INTO Motorista VALUES('121.334.543-24', 'Marcos', 'marcos@uol.com', 84782478, 5678);
-INSERT INTO Motorista VALUES('645.788.546-45', 'Roger', 'roger@aol.com', 17654378, 5678);
+INSERT INTO Motorista VALUES('10293847560', 'Jorge', 'jorge@bol.com', 12443857, 1234);
+INSERT INTO Motorista VALUES('12133454324', 'Marcos', 'marcos@uol.com', 84782478, 5678);
+INSERT INTO Motorista VALUES('64578854645', 'Roger', 'roger@aol.com', 17654378, 5678);
 
-INSERT INTO Corrida VALUES('123.456.789-10', '102.938.475-60', 1, now(), now(), 'Aqui', 'Ali');
-INSERT INTO Corrida VALUES('987.654.321-00', '121.334.543-24', 2, now(), now(), 'Ali', 'Aqui');
+INSERT INTO Corrida VALUES('12345678910', '102.938.475-60', 1, now(), now(), 'Aqui', 'Ali');
+INSERT INTO Corrida VALUES('98765432100', '121.334.543-24', 2, now(), now(), 'Ali', 'Aqui');
 
 SELECT * FROM Passageiro;
 SELECT * FROM Categoria;
 SELECT * FROM Carro;
 SELECT * FROM Motorista;
 SELECT * FROM Corrida;
-
